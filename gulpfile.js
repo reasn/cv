3faaeae393a2c3bc3361f39e4b7ee48c96ac3f64
Alexander Thiel <me@alexthiel.de>
2014-12-27 14:25:16 +0100

    gulp improvements

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,28 @@
-var gulp        = require('gulp'),
-    typescript  = require('gulp-typescript'),
-    concat      = require('gulp-concat-sourcemap'),
-    sourcemaps  = require('gulp-sourcemaps'),
-    copy        = require('gulp-copy'),
-    less        = require('gulp-less'),
-    eventStream = require('event-stream');
+var gulp            = require('gulp'),
+    gulpLoadPlugins = require('gulp-load-plugins'),
+    plugins         = gulpLoadPlugins({lazy: false});
+/*
+ typescript   = require('gulp-typescript'),
+ concat       = require('gulp-concat-sourcemap'),
+ sourcemaps   = require('gulp-sourcemaps'),
+ copy         = require('gulp-copy'),
+ less         = require('gulp-less'),
+ autoprefixer = require('gulp-autoprefixer'),
+ minifycss    = require('gulp-minify-css'),
+ rename       = require('gulp-rename'),
+ eventStream  = require('event-stream');
+ */
 
 gulp.task('style', function () {
     gulp.src('./client/style/style.less')
-        .pipe(less({}))
-        .pipe(gulp.dest('./dist'));
+        .pipe(plugins.less({}))
+        .pipe(plugins.autoprefixer('> 5%', 'last 2 versions'))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(plugins.rename({suffix: '.min'}))
+        .pipe(plugins.minifyCss())
+        .pipe(gulp.dest('./dist/css'));
 });
-
-var tsProject = typescript.createProject({
+var tsProject = plugins.typescript.createProject({
     declarationFiles:  true,
     noExternalResolve: true,
     sortOutput:        true
@@ -23,25 +33,26 @@ gulp.task('script', function () {
         './client/script/**/*.ts',
         './typings/**/*.d.ts'
     ]).
-        pipe(typescript(tsProject));
+        pipe(plugins.typescript(tsProject));
 
-    return tsResult.js.pipe(concat('script.js')) // You can use other plugins that also support gulp-sourcemaps
-        .pipe(sourcemaps.write()) // Now the sourcemaps are added to the .js file
-        .pipe(gulp.dest('./dist'));
-    /*
-     return eventStream.merge(
-     tsResult.dts.pipe(gulp.dest('dist/script')),
-     tsResult.js.pipe(gulp.dest('dist/script'))
-     );*/
+    return tsResult.js
+        .pipe(plugins.concatSourcemap('cv.js')) // You can use other plugins that also support gulp-sourcemaps
+        .pipe(plugins.sourcemaps.write()) // Now the sourcemaps are added to the .js file
+        .pipe(plugins.jshint.reporter('default'))
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(plugins.rename({suffix: '.min'}))
+        .pipe(plugins.uglify())
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(plugins.notify({message: 'Scripts task complete'}));
 });
 
 gulp.task('view', function () {
     gulp.src('./client/view/index.html')
-        .pipe(copy('./dist', {prefix: 2}));
+        .pipe(plugins.copy('./dist', {prefix: 2}));
 });
 gulp.task('assets', function () {
     gulp.src('./client/assets/**/*.*')
-        .pipe(copy('./dist/assets', {prefix: 2}));
+        .pipe(plugins.copy('./dist/assets', {prefix: 2}));
 });
 gulp.task('watch', ['script', 'style', 'view', 'assets'], function () {
     gulp.watch('./gulpfile.js', ['watch']);
